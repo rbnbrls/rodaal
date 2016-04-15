ea0f19ddb5b125d333d9fe33b1bc2c2a7a4683cd
rbnbrls <rubenbarels@gmail.com>
2016-04-15 19:23:52 +0200

    added bruto

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 salaris = 2550
-invoer = int(input("Wat is je salaris?"))
+invoer = int(input("Wat is je bruto salaris?"))
 rodaal = invoer/salaris
 if rodaal > 1:
     rodaal = (rodaal - 1)*100
-    print("Gefeliciteerd, je salaris is",round(float(rodaal), 2),"% boven rodaal.")
+    print("Gefeliciteerd, je salaris is", round(float(rodaal), 2),"% boven rodaal.")
 else:
     rodaal = (1 - rodaal)*100
     print("Helaas, je salaris ligt",round(float(rodaal), 2),"% onder rodaal.")
